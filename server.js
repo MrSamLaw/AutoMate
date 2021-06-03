5734a2c346803a7153353d98089c63f773ddb364
Mr Sam Law <sam@chongland.com>
2021-06-03 18:53:10 +1000

    Remove EJS, use Handlebars

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
+const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 
@@ -10,6 +11,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const hbs = exphbs.create({helpers});
 
 const sess = {
   secret: 'Super secret secret',
@@ -24,7 +26,8 @@ const sess = {
 app.use(session(sess));
 
 // Inform Express.js on which template engine to use
-app.set('view engine', 'ejs');
+app.engine('handlebars',hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
