0570d5f6289b27a819eb4170dcd7cc0d9518be74
Mr Sam Law <sam@chongland.com>
2021-06-10 18:53:44 +1000

    starter staffRoutes

--- controllers/api/staffRoutes.js
+++ controllers/api/staffRoutes.js
@@ -1,50 +1,70 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { Staff } = require('../../models');
 
 router.post('/', async (req, res) => {
   try {
-    const userData = await User.create(req.body);
+    const staffData = await Staff.create(req.body);
 
     req.session.save(() => {
-      req.session.user_id = userData.id;
+      req.session.staff_id = staffData.id;
       req.session.logged_in = true;
 
-      res.status(200).json(userData);
+      res.status(200).json(staffData);
     });
   } catch (err) {
     res.status(400).json(err);
   }
 });
 
-router.post('/login', async (req, res) => {
+router.post('/stafflogin', async (req, res) => {
   try {
-    const userData = await User.findOne({ where: { email: req.body.email } });
+    const staffData = await Staff.findOne({ where: { username: req.body.email } });
 
-    if (!userData) {
+    if (!staffData) {
       res
         .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
+        .json({ message: 'Incorrect username or password, please try again' });
       return;
     }
 
-    const validPassword = await userData.checkPassword(req.body.password);
+    const validPassword = await staffData.checkPassword(req.body.password);
 
     if (!validPassword) {
       res
         .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
+        .json({ message: 'Incorrect username or password, please try again' });
       return;
     }
 
     req.session.save(() => {
-      req.session.user_id = userData.id;
+      req.session.staff_id = staffData.id;
       req.session.logged_in = true;
+      req.session.staff = true;
       
-      res.json({ user: userData, message: 'You are now logged in!' });
+      res.json({ staff: staffData, message: 'You are now logged in!' });
     });
 
   } catch (err) {
     res.status(400).json(err);
+  }
+});
+
+router.get('staff/:id', async (req, res) => {
+  try {
+    const staffData = await Staff.findByPk(req.params.id, {
+      attributes: [],
+      include: [{},],
+    });
+
+    const staff = staffData.get({plain: true});
+
+    res.render('staffdash', {
+      ...staff,
+      logged_in: req.session.logged_in,
+      staff: req.session.staff
+    });
+  } catch(err) {
+    res.status(500).json(err);
   }
 });
 
