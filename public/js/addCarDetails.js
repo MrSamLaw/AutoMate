d0f3c8ae86658be9681704764f1a21f622db1ce1
Mr Sam Law <sam@chongland.com>
2021-06-14 12:50:49 +1000

    begin AddCarDetails functionality

--- public/js/addCarDetails.js
+++ public/js/addCarDetails.js
@@ -1,59 +1,36 @@
-    const rego = document.querySelector('#carRego').value;
-    const make = document.querySelector('#carMake').value;
-    const model = document.querySelector('#carModel').value;
-    const year = document.querySelector('#carYear').value;
-    const kms = document.querySelector('#carKMs').value;
-
-    const vehicleResponse = await fetch('/api/vehicle', {
-        method: 'POST',
-        body: JSON.stringify({
-            rego,
-        make,
-        model,
-        year,
-        kms
-    }),
-        headers: {
-         'Content-Type': 'application/json'
-        }
-    });
-
-    const { doc } = require("prettier");
-
 async function newFormHandler(event) {
-    console.log("got hereeeee");
-    event.preventDefault();
+  event.preventDefault();
 
-    const rego = document.querySelector('#carRego').value;
-    const make = document.querySelector('#carMake').value;
-    const model = document.querySelector('#carModel').value;
-    const year = document.querySelector('#carYear').value;
-    const kms = document.querySelector('#carKMs').value;
-    
+  const rego = document.querySelector('#carRego').value;
+  const make = document.querySelector('#carMake').value;
+  const model = document.querySelector('#carModel').value;
+  // const year = document.querySelector('#carYear').value;
+  const kms = document.querySelector('#carKMs').value;
 
-    console.log(make, model, year);
 
+  console.log(make, model, year);
+  if (rego && make && model && kms) {
     const vehicleResponse = await fetch(`/api/vehicle`, {
       method: 'POST',
       body: JSON.stringify({
         rego,
         make,
         model,
-        year,
+        // year,
         kms
-    }),
+      }),
       headers: {
         'Content-Type': 'application/json',
       },
     });
 
-    if (response.ok) {
+    if (vehicleResponse.ok) {
       document.location.replace('/');
     } else {
       alert('Failed to add vehicle');
     }
   }
-
-  document
-    .querySelector('.vehicle-form')
-    .addEventListener('submit', newFormHandler); 
+};
+document
+  .querySelector('.vehicle-form')
+  .addEventListener('submit', newFormHandler);
