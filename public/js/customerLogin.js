f52e07de943ff97519f5bf84dd987c5ded794b4f
Mr Sam Law <sam@chongland.com>
2021-06-15 13:02:43 +1000

    Fixed customer login & redirects to customer Dash

--- public/js/customerLogin.js
+++ public/js/customerLogin.js
@@ -6,19 +6,20 @@ async function newFormHandler(event) {
   const email = document.querySelector('#email').value;
   const password = document.querySelector('#inputPassword').value;
 
-if(email && password) {
-  const response = await fetch('/api/customer/login', {
-    method: 'POST',
-    body: JSON.stringify({ email, password }),
-    headers: { 'Content-Type': 'application/json' },
-  });
+  if (email && password) {
+    const response = await fetch('/api/customer/login', {
+      method: 'POST',
+      body: JSON.stringify({ email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
 
-  if (response.ok) {
-    document.location.replace('/');
-  } else {
-    alert('Failed to log in.');
+    if (response.ok) {
+      const resBody = await response.json();
+      document.location.replace(`/api/customer/${resBody.customer.id}`);
+    } else {
+      alert('Failed to log in.');
+    }
   }
-}
 };
 document
   .querySelector('.loginform')
