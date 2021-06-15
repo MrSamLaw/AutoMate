06dcb214a642b85d5fb7f4e5a9621b189d4e883d
Jessica Luff <78363096+JessLuff@users.noreply.github.com>
2021-06-15 15:30:36 +1000

    custDash adding jobs

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,10 +2,13 @@ const router = require('express').Router();
 const vehicleRoutes = require('./vehicleRoutes');
 const staffRoutes = require('./staffRoutes');
 const customerRoutes = require('./customerRoutes');
+const jobRoutes = require('./jobRoutes');
 
 
 router.use('/vehicle', vehicleRoutes);
 router.use('/staff', staffRoutes);
 router.use('/customer', customerRoutes);
+router.use('/job', jobRoutes);
+//router.use('/jobs', jobRoutes);
 
 module.exports = router;
